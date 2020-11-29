dc30922767b6c6c334bca720b51cbcc2c1725ea6
Jason <jasonbrown@mailbox.org>
2020-11-29 12:16:25 -0500

    completed to step 7

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http'
+import { Http, Response } from '@angular/http';
+import { Album } from '../app/album';
 import 'rxjs/add/operator/map';
 
 @Injectable()
