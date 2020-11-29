181085ffca46351c7938ebb8b8a6c9b733c0d783
Jason <jasonbrown@mailbox.org>
2020-11-29 14:32:58 -0500

    finished the project?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,11 @@ import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component'
 
-const appRoutes: Routes[];
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductListComponent },
+  { path: 'product/:id', component: ProductPageComponent }
+];
 
 @NgModule({
   declarations: [
@@ -24,7 +28,8 @@ const appRoutes: Routes[];
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule    
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
